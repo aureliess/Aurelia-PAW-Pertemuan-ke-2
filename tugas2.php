ddab694f023472a745b62f525c54dca0ce8e407c
aureliess <aureliayasmine110@gmail.com>
2022-08-30 20:06:12 +0700

    output pertemuan 2

---
+++ tugas2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Output Tugas 2</title>
+<body>
+<h2> Aurelia Putri Yasmine</h2>
+<h3> 210411100155 </h4>
+<h2> ## Tugas Output Pertemuan 2 ##</h2>
+<?php
+    function nama($bayi1, $bayi2, $bayi3) {
+        echo "<br> Bayi bernama $bayi1, lebih tinggi daripada bayi bernama $bayi2 dan lebih pendek dari bayi bernama $bayi3.</br>";
+    }
+    nama('Ana','Tika', 'Mila');
+    nama('Arista', 'Mawar', 'Dita');
+    nama('Fara', 'Lia', 'Melati');
+   
+?>
+
+</body>
+</head>
+</html>
